f3c5200a69b8c6a8d9e34a6e97ee0c22f74d0009
Said Özcan <saidozcn@gmail.com>
2015-01-01 18:06:41 +0100

    Added dynamic version text and changed version number for new version submit

--- 5Artists/Classes/About/TWLAboutViewController.m
+++ 5Artists/Classes/About/TWLAboutViewController.m
@@ -24,17 +24,22 @@
     
 
     // Create the attributed string
-    NSMutableAttributedString *aboutString = [[NSMutableAttributedString alloc]initWithString:
-                                              @"\n\n\n\n\nThis application is built by Said ÖZCAN (@saidozcan) using Spotify iOS SDK.\n\nFeel free to email us at info@towerlabs.co\n\n© 2014 Tower Labs. — version 1.0"];
+    NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
+    NSString *version = [infoDict objectForKey:@"CFBundleShortVersionString"];
+    
+    NSCalendar *calendar = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    NSInteger year = [calendar component:NSCalendarUnitYear fromDate:[NSDate date]];
+    
+    NSString *about = [NSString stringWithFormat:@"\n\n\n\n\nThis application is built by Said ÖZCAN (@saidozcan) using Spotify iOS SDK.\n\nFeel free to email us at info@towerlabs.co\n\n© %d Tower Labs. — version %@\n\n", (int)year, version];
+    NSMutableAttributedString *aboutString = [[NSMutableAttributedString alloc]initWithString:about];
+    
+    NSRange rangeOfAboutString = NSMakeRange(0, [aboutString length]);
     
     // Declare the fonts
     UIFont *aboutStringFont1 = [UIFont fontWithName:@"AvenirNext-Regular" size:14.0];
-    UIFont *aboutStringFont2 = [UIFont fontWithName:@"Helvetica-Oblique" size:14.0];
     
     // Declare the colors
-    UIColor *aboutStringColor1 = [UIColor colorWithRed:0.000000 green:0.000000 blue:0.000000 alpha:0.360784];
-    UIColor *aboutStringColor2 = [UIColor colorWithWhite:1.000000 alpha:1.000000];
-    UIColor *aboutStringColor3 = [UIColor colorWithRed:0.109804 green:0.109804 blue:0.109804 alpha:1.000000];
+    UIColor *aboutStringColor1 = [UIColor colorWithWhite:1.000000 alpha:1.000000];
     
     // Declare the paragraph styles
     NSMutableParagraphStyle *aboutStringParaStyle1 = [[NSMutableParagraphStyle alloc]init];
@@ -42,22 +47,10 @@
     
     
     // Create the attributes and add them to the string
-    [aboutString addAttribute:NSParagraphStyleAttributeName value:aboutStringParaStyle1 range:NSMakeRange(0,145)];
-    [aboutString addAttribute:NSUnderlineColorAttributeName value:aboutStringColor1 range:NSMakeRange(0,145)];
-    [aboutString addAttribute:NSFontAttributeName value:aboutStringFont1 range:NSMakeRange(0,145)];
-    [aboutString addAttribute:NSForegroundColorAttributeName value:aboutStringColor2 range:NSMakeRange(0,145)];
-    [aboutString addAttribute:NSForegroundColorAttributeName value:aboutStringColor2 range:NSMakeRange(145,1)];
-    [aboutString addAttribute:NSFontAttributeName value:aboutStringFont2 range:NSMakeRange(145,1)];
-    [aboutString addAttribute:NSKernAttributeName value:[NSNumber numberWithInteger:0] range:NSMakeRange(145,1)];
-    [aboutString addAttribute:NSParagraphStyleAttributeName value:aboutStringParaStyle1 range:NSMakeRange(145,1)];
-    [aboutString addAttribute:NSStrokeColorAttributeName value:aboutStringColor3 range:NSMakeRange(145,1)];
-    [aboutString addAttribute:NSForegroundColorAttributeName value:aboutStringColor2 range:NSMakeRange(146,1)];
-    [aboutString addAttribute:NSParagraphStyleAttributeName value:aboutStringParaStyle1 range:NSMakeRange(146,1)];
-    [aboutString addAttribute:NSFontAttributeName value:aboutStringFont1 range:NSMakeRange(146,1)];
-    [aboutString addAttribute:NSParagraphStyleAttributeName value:aboutStringParaStyle1 range:NSMakeRange(147,11)];
-    [aboutString addAttribute:NSUnderlineColorAttributeName value:aboutStringColor1 range:NSMakeRange(147,11)];
-    [aboutString addAttribute:NSFontAttributeName value:aboutStringFont1 range:NSMakeRange(147,11)];
-    [aboutString addAttribute:NSForegroundColorAttributeName value:aboutStringColor2 range:NSMakeRange(147,11)];
+    [aboutString addAttribute:NSLigatureAttributeName value:[NSNumber numberWithInteger:0] range:rangeOfAboutString];
+    [aboutString addAttribute:NSParagraphStyleAttributeName value:aboutStringParaStyle1 range:rangeOfAboutString];
+    [aboutString addAttribute:NSFontAttributeName value:aboutStringFont1 range:rangeOfAboutString];
+    [aboutString addAttribute:NSForegroundColorAttributeName value:aboutStringColor1 range:rangeOfAboutString];
     _aboutLabel.attributedText = aboutString;
 }
 
